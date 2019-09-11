48de26467225ca7d148424f88ea8280280bce4aa
rufongabrillojr <rufo.gabrillo@bliimo.com>
2019-09-11 15:30:54 +0800

    Add fonts

--- src/components/Tabs/Dashboard/Activity.jsx
+++ src/components/Tabs/Dashboard/Activity.jsx
@@ -16,7 +16,19 @@ import SaveMoney from '../../../img/HomePage/SaveMoney.png';
 export default class Activity extends React.Component {
 
   state = {
-    display: 'dashboard'
+    display: 'dashboard', 
+    description: `<h1 style='font-family: var(--font-light)'>The <span style='font-family: var(--font-bold)'>Talk</span></h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h1 style='font-family: var(--font-light)'>The <span style='font-family: var(--font-bold)'>Place</span></h1><p>
+    <div class="swiper-container swiper-init demo-swiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide"><img src='${DigitalRevolution}' alt=''></div>
+        <div class="swiper-slide"><img src='${InvestoMania}' alt=''></div>
+        <div class="swiper-slide"><img src='${MPossible}' alt=''></div>
+        <div class="swiper-slide"><img src='${MillenialTalks}' alt=''></div>
+        <div class="swiper-slide"><img src='${SaveMoney}' alt=''></div>
+      </div>
+    </div>
+    </p>`
   }
 
   componentWillMount = () => {
@@ -27,17 +39,16 @@ export default class Activity extends React.Component {
 
   render(){
     return(
-      <div className='container'>
-        <img src={thumbnail} alt=' bg image' />
-        <div className=''>
-          <img src={MPossibleLogo} alt='logo' />
-          <img src={profilePicture} alt='profile picture' />
-          <p className='makeMeRed'>Jane Dela Cruz</p>
+      <div className='container activity'>
+        <Link iconMd='material:keyboard_arrow_left' color="white" className='back-button' onClick={ () => { this.props.event(true) } }></Link>
+        <img src={thumbnail} alt=' bg image' className='bg-image' />
+        <div className='profile'>
+          <img src={MPossibleLogo} alt='logo' className='logo' />
+          <img src={profilePicture} alt='profile picture' className='picture' />
+          <p className='name'>Jane Dela Cruz</p>
         </div>
-        <div className='description'>
-        </div>
+        <div className='description' dangerouslySetInnerHTML={{__html: this.state.description}} ></div>
         <div className='get-data-from-user'>
-
         <List>
           <ListInput
             type="textarea"
