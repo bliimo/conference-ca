496d1653e525949cf1ba0958c905fbeb295b9b45
Jonel <jonel01sandeigo@gmail.com>
2019-09-11 19:07:46 +0800

    WIP

--- src/components/Tabs/Dashboard/Activity.jsx
+++ src/components/Tabs/Dashboard/Activity.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Swiper, SwiperSlide, Link, List, ListInput, Icon } from 'framework7-react';
+import { Swiper, SwiperSlide, Link, List, ListInput, Icon, ListItem, AccordionContent, Block } from 'framework7-react';
 
 import style from './style.css';
 
@@ -12,6 +12,8 @@ import InvestoMania from '../../../img/HomePage/InvestoMania.png';
 import MPossible from '../../../img/HomePage/MPossible.png';
 import MillenialTalks from '../../../img/HomePage/MillenialTalks.png';
 import SaveMoney from '../../../img/HomePage/SaveMoney.png';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 export default class Activity extends React.Component {
 
@@ -28,43 +30,112 @@ export default class Activity extends React.Component {
   render(){
     return(
       <div className='container'>
-        <img src={thumbnail} alt=' bg image' />
-        <div className=''>
-          <img src={MPossibleLogo} alt='logo' />
-          <img src={profilePicture} alt='profile picture' />
-          <p className='makeMeRed'>Jane Dela Cruz</p>
+        <img src={thumbnail} alt=' bg image' className='bgImage'/>
+        <div className='imagePos'>
+          <img src={ MPossibleLogo } alt='logo' className='mpossibleLogo'/>
+          <img src={ profilePicture } alt='profile picture' className='profile'/>
+          <p className='name'>Jane Dela Cruz</p>
+        </div>
+        <div className='title'>
+          <span className='theTalk'>The <strong>Talk</strong></span>
         </div>
         <div className='description'>
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          </span><br/>
+          <span className='secondSentence'>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, 
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </span>
         </div>
-        <div className='get-data-from-user'>
+        <div className='thePlace'>
+          <span>The <strong>Place</strong></span>
+        </div>
+        <div>
+            <Carousel showArrows={true}
+             onChange={this.onChange} 
+             onClickItem={this.onClickItem} 
+             classname='carousel'>
+                <div>
+                    <img src={ DigitalRevolution }/>
+                    <span className="legend">First Image</span>
+                </div>
+                <div>
+                    <img src={ InvestoMania } />
+                    <span className="legend">Second Image</span>
+                </div>
+                <div>
+                    <img src={ MPossible } />
+                    <span className="legend">Third Image</span>
+                </div>
+                <div>
+                    <img src={ MillenialTalks } />
+                    <span className="legend">Fourth Image</span>
+                </div>
+                <div>
+                    <img src={ SaveMoney } />
+                    <span className="legend">Fifth Image</span>
+                </div>
+            </Carousel>
+        </div>
 
-        <List>
-          <ListInput
-            type="textarea"
-            placeholder="Comment"
-            clearButton
-            resizable
-          />
-          <ListInput
-            type="textarea"
-            placeholder="Suuggestion"
-            clearButton
-            resizable
-          />
-          <ListInput
-            type="textarea"
-            placeholder="Question"
-            clearButton
-            resizable
-          />
-          <div className=''>
-          <Icon material='star' />
-          <Icon material='star' />
-          <Icon material='star_half' />
-          <Icon material='star_border' />
-          <Icon material='star_border' />
+        <div className='get-data-from-user'>
+          <List accordionList className='accordionContainer'>
+            <ListItem accordionItem title="Comment" className='backgroundColor'>
+              <AccordionContent>
+                <Block>
+                  <ListInput
+                    className='textArea'
+                    type="textarea"
+                    placeholder="Comment"
+                    clearButton
+                    resizable
+                  />
+                </Block>
+              </AccordionContent>
+            </ListItem>
+
+            <ListItem accordionItem title="Suggestion" className='backgroundColor'>
+              <AccordionContent>
+                <Block>
+                  <ListInput
+                    className='textArea'
+                    type="textarea"
+                    placeholder="Suggestion"
+                    clearButton
+                    resizable
+                  />
+                </Block>
+              </AccordionContent>
+            </ListItem>
+            
+            <ListItem accordionItem title="Question" className='backgroundColor'>
+              <AccordionContent>
+                <Block>
+                  <ListInput
+                    className='textArea'
+                    type="textarea"
+                    placeholder="Question"
+                    clearButton
+                    resizable
+                  />
+                </Block>
+              </AccordionContent>
+            </ListItem>
+          </List>
+ 
+          <div className='stars'>
+            <Icon material='star' />
+            <Icon material='star' />
+            <Icon material='star_half' />
+            <Icon material='star_border' />
+            <Icon material='star_border' />
           </div>
-        </List>
+
+          <div>
+            <br/>
+          </div>
 
         </div>
       </div>
