d2cad6fd375589843180ac93ebd7ab596c6bc4ed
Erwin Doria <erwin.doria.rnr@gmail.com>
2019-09-22 18:07:50 +0800

    Login and Registration functionality firebase

--- src/components/pages/LoginPage/Page.jsx
+++ src/components/pages/LoginPage/Page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Page, Link, Toolbar, Tab, Tabs } from 'framework7-react';
 
 import Dashboard from '../../Tabs/Dashboard/Dashboard';
@@ -11,8 +11,49 @@ import DashboardIcon from '../../../img/icons/activities.png';
 import BoothsIcon from '../../../img/icons/booths.png';
 import AccountIcon from '../../../img/icons/account.png';
 
-export default () => (
-  <Page pageContent={false}>
-    Login Page
-  </Page>
-);
+import { connect } from 'react-redux';
+import { auth } from '../../../reducers/reducer';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: 'erwin.doria@bliimo.com',
+      password: '12345678'
+    };
+  }
+
+  HandleLogin = async () => {
+    const { email, password } = this.state;
+    auth(email, password)
+      .then(e => {
+        console.log(e);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  componentDidMount() {
+    this.HandleLogin();
+  }
+
+  render() {
+    return <Page pageContent={false}>Login Page</Page>;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    authState: state.authState
+  };
+};
+
+const mapDispatchToProps = {
+  auth
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
--- src/components/pages/RegistrationPage/Page.jsx
+++ src/components/pages/RegistrationPage/Page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Page, Link, Toolbar, Tab, Tabs } from 'framework7-react';
 
 import Dashboard from '../../Tabs/Dashboard/Dashboard';
@@ -11,8 +11,65 @@ import DashboardIcon from '../../../img/icons/activities.png';
 import BoothsIcon from '../../../img/icons/booths.png';
 import AccountIcon from '../../../img/icons/account.png';
 
-export default () => (
-  <Page pageContent={false}>
-    Registration Page
-  </Page>
-);
+import { connect } from 'react-redux';
+import { addAuth, setData } from '../../../reducers/reducer';
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: 'erwin.doria@bliimo.com',
+      password: '12345678',
+      firstname: 'Erwin',
+      middlename: 'Barayuga',
+      lastname: 'Doria'
+    };
+  }
+
+  onHandleRegister = () => {
+    let { email, password, firstname, middlename, lastname } = this.state;
+    addAuth(email, password)
+      .then(e => {
+        console.log(e, e.response);
+        if (e.response == 'success') {
+          let userData = { accountID: e.id, firstname, middlename, lastname, email, status: false, profilePicture: '' };
+          setData(`user/${e.id}`, userData)
+            .then(e => {
+              setTimeout(() => {
+                console.log('Registration successful!');
+              }, 1000);
+              this.setState({ signing: false, email: '', mobilenumber: '', password: '', firstname: '', middlename: '', lastname: '', username: '' });
+            })
+            .catch(e => {
+              console.log('Something went wrong', e);
+            });
+        } else if (e.error.code == 'auth/network-request-failed') {
+          console.log('No internet');
+        } else if (e.error.code == 'auth/email-already-in-use') {
+          console.log('Email already used !');
+        }
+      })
+      .catch(e => {
+        console.log('Something went wrong', e);
+      });
+  };
+
+  render() {
+    return <Page pageContent={false}>Registration Page</Page>;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    authState: state.authState
+  };
+};
+
+const mapDispatchToProps = {
+  addAuth
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Register);
