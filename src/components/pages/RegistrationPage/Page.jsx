58e0c8e99061467da5b9ba7540ecf78c94754798
noe.fabellon <noe.fabellon@bliimo.com>
2019-09-22 18:18:12 +0800

    Remove unused imports

--- src/components/pages/RegistrationPage/Page.jsx
+++ src/components/pages/RegistrationPage/Page.jsx
@@ -1,15 +1,5 @@
 import React from 'react';
-import { Page, Link, Toolbar, Tab, Tabs } from 'framework7-react';
-
-import Dashboard from '../../Tabs/Dashboard/Dashboard';
-import Booth from '../../Tabs/Booths/Booth';
-import Account from '../../Tabs/Account/Account';
-
-import style from './style.css';
-
-import DashboardIcon from '../../../img/icons/activities.png';
-import BoothsIcon from '../../../img/icons/booths.png';
-import AccountIcon from '../../../img/icons/account.png';
+import { Page } from 'framework7-react';
 
 export default () => (
   <Page pageContent={false}>
