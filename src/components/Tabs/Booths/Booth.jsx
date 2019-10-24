84c373f62c910c23628aeb79883d45fcd8f7c8e4
Erwin Doria <erwin.doria.rnr@gmail.com>
2019-10-24 18:28:14 +0800

    add website and contact

--- src/components/Tabs/Booths/Booth.jsx
+++ src/components/Tabs/Booths/Booth.jsx
@@ -1,36 +1,65 @@
 import React from 'react';
-import { Navbar, NavLeft, NavTitle, Block, AccordionItem, AccordionContent, AccordionToggle, Link } from 'framework7-react';
+import {
+  Navbar,
+  NavLeft,
+  NavTitle,
+  Block,
+  AccordionItem,
+  AccordionContent,
+  AccordionToggle,
+  Link
+} from 'framework7-react';
 
 import style from './style.css';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import Dotdotdot from 'react-dotdotdot';
 import { getData, api } from '../../../reducers/reducer';
 
-const DisplayBooth = (props) =>{
+const DisplayBooth = props => {
   return (
-    <div className='booth-wrapper'>
-      <img className='booth-img' src={props.data.logo} alt={props.data.booth} />
-      <p className='company-name'>{props.data.company}</p>
-      <div className='company-desc'>{props.data.description}</div>
+    <div className="booth-wrapper">
+      <img className="booth-img" src={props.data.logo} alt={props.data.booth} />
+      <p className="company-name">{props.data.company}</p>
+      <div className="company-desc">{props.data.description}</div>
+      <div style={{textAlign:'left',marginTop:'1em' }}>
+        <div>
+          <span>Website: </span>
+          <Link href={props.data.website} external target="_blank">
+            {props.data.website}
+          </Link>
+        </div>
+        <div>
+          <span>Contact: </span>
+          <Link href={`tel:${props.data.phone}`} external target="_blank">
+            {props.data.phone}
+          </Link>
+        </div>
+      </div>
     </div>
-  )
-
-}
+  );
+};
 const HandleDisplayBooth = props => {
-  const htmlRender = () =>{
-    return {__html:props.data.description}
-  }
+  const htmlRender = () => {
+    return { __html: props.data.description };
+  };
   return (
     <AccordionItem>
       <AccordionToggle>
-        <div className="booth-information" onClick={()=>{props.click('booth',props.data)}}>
+        <div
+          className="booth-information"
+          onClick={() => {
+            props.click('booth', props.data);
+          }}
+        >
           <div className="left">
             <img src={props.data.logo} alt={props.data.booth} />
           </div>
           <div className="right">
-            <div className="booth-name" style={{fontSize:'1.2em',marginTop:'.5em'}}>{props.data.company}</div>
+            <div className="booth-name" style={{ fontSize: '1.2em', marginTop: '.5em' }}>
+              {props.data.company}
+            </div>
             <Dotdotdot clamp={1}>
-              <div className="booth-description" dangerouslySetInnerHTML={htmlRender()}/>
+              <div className="booth-description" dangerouslySetInnerHTML={htmlRender()} />
             </Dotdotdot>
           </div>
         </div>
@@ -58,13 +87,13 @@ const HandleDisplayBooth = props => {
 class HomePage extends React.Component {
   state = {
     booths: [],
-    boothOpen:{},
-    display:'main'
+    boothOpen: {},
+    display: 'main'
   };
 
   getDataFromApi = async () => {
-    const data = await this.props.getBooths('booths.json')
-    this.setState({booths:data.payload.data})
+    const data = await this.props.getBooths('booths.json');
+    this.setState({ booths: data.payload.data });
   };
 
   componentWillMount = () => {
@@ -73,36 +102,47 @@ class HomePage extends React.Component {
 
   componentDidMount = () => {};
 
-  handleClick = (display,boothOpen) =>{
-    this.setState({display,boothOpen})
-  }
+  handleClick = (display, boothOpen) => {
+    this.setState({ display, boothOpen });
+  };
 
   render() {
     return (
       <div className="booths">
         <Navbar className="nav-booths">
-        <Link
-          iconMd="material:keyboard_arrow_left"
-          color="white"
-          className="back-button"
-          tabLink={`${this.state.display === 'main' ? '#dashboard':'#'}`}
-          onClick={()=>{ this.state.display === 'booth' ? this.setState({display:'main'}): console.log('main')}}
-          
-        ></Link>
-          <NavTitle className="top-title">{this.state.display === 'main' ? 'Activities/Booths': 'Booths'}</NavTitle>
+          <Link
+            iconMd="material:keyboard_arrow_left"
+            color="white"
+            className="back-button"
+            tabLink={`${this.state.display === 'main' ? '#dashboard' : '#'}`}
+            onClick={() => {
+              this.state.display === 'booth'
+                ? this.setState({ display: 'main' })
+                : console.log('main');
+            }}
+          ></Link>
+          <NavTitle className="top-title">
+            {this.state.display === 'main' ? 'Activities/Booths' : 'Booths'}
+          </NavTitle>
         </Navbar>
 
         <Block inner accordionList>
-          {this.state.display === 'main' && Object.keys(this.state.booths).map((booth, index) => {
-            return <HandleDisplayBooth click={this.handleClick} key={index} data={this.state.booths[booth]} />
-          })}
-          {this.state.display === 'booth' && <DisplayBooth data={this.state.boothOpen}/>}
+          {this.state.display === 'main' &&
+            Object.keys(this.state.booths).map((booth, index) => {
+              return (
+                <HandleDisplayBooth
+                  click={this.handleClick}
+                  key={index}
+                  data={this.state.booths[booth]}
+                />
+              );
+            })}
+          {this.state.display === 'booth' && <DisplayBooth data={this.state.boothOpen} />}
         </Block>
       </div>
     );
   }
 }
-
 
 const mapStateToProps = state => {
   return {
@@ -110,11 +150,12 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchToProps =(dispatch)=>{
+const mapDispatchToProps = dispatch => {
   return {
-    getBooths:(url)=>{ return dispatch(api(url)) },
-
-  }
+    getBooths: url => {
+      return dispatch(api(url));
+    }
+  };
 };
 
 export default connect(
